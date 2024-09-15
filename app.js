0db0ee149fa3e5ab19dcfd1828238a85979b3e28
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-09-15 21:32:32 +0100

    Add route to get selected files

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ const path = require('path');
 const app = express();
 const PORT = 2345;
 const FILE_DIRECTORY = path.join(__dirname, 'shared_files');
+
+// This shall Serve static HTML/CSS/JS files for the web interface
+app.use(express.static('public'));
+
+// API route to fetch available files (selected ofcourse)
+app.get('/api/files', (req, res) => {
+    fs.readdir(FILE_DIRECTORY, (err, files) => {
+        if (err) {
+            return res.status(500).json({ error: 'Unable to list files' });
+        }
+        res.json(files);
+    });
+});
