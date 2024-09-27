856f9d3ec98351c05bff2e5f30edf164794ed3b0
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-09-27 13:13:25 +0100

    Create achieve api host

---
+++ public/components/achieve/index.js
@@ -0,0 +1,24 @@
+export const newEvent=(eventName, customData = {})=> {
+    const payload = {
+        eventName: eventName, 
+        timestamp: new Date().toISOString(), 
+        customData: customData
+    };
+
+    // Send the payload to your backend
+    fetch('https://achieve-api.vercel.app/v0/track', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    })
+    .then(response => {
+        if (!response.ok) {
+            // throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => null)
+    .catch(error => null);
+}
